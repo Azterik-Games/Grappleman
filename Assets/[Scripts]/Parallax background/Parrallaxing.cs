916b6281a300c73ac5e171c2c932fecfae2b7550
Grootloops33 <43176241+Grootloops33@users.noreply.github.com>
2018-11-18 14:41:55 +0100

    Implemented Parallaxing basics

---
+++ Assets/[Scripts]/Parallax background/Parrallaxing.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Azterik.Mechanics.Parallaxing
+{
+    public class Parrallaxing : MonoBehaviour
+    {
+        [SerializeField] Transform[] backgrounds;
+        float[] parallaxScales;
+        [SerializeField] float smoothing = 1f;
+
+        Transform cam;
+        Vector3 previousCamPos;
+
+        void Start()
+        {
+            cam = Camera.main.transform;
+
+            previousCamPos = cam.position;
+
+            parallaxScales = new float[backgrounds.Length];
+            for (int i = 0; i < backgrounds.Length; i++)
+            {
+                parallaxScales[i] = backgrounds[i].position.z * -1;
+            }
+        }
+
+        void Update()
+        {
+            for (int i = 0; i < backgrounds.Length; i++)
+            {
+                float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
+                float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+
+                Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+                backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+            }
+
+            previousCamPos = cam.position;
+        }
+    }
+}
---
+++ Assets/[Scripts]/Parallax background/Tiling.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Azterik.Mechanics.Parallaxing
+{
+    public class Tiling : MonoBehaviour
+    {
+        [SerializeField] Vector2 offset = new Vector2(2, 0);
+
+        bool rightNeighbour = false;
+        bool leftNeighbour = false;
+
+        bool reverseScale = false;
+
+        float width = 0f;
+        Camera cam;
+
+        void Start()
+        {
+            SpriteRenderer s = GetComponent<SpriteRenderer>();
+            width = s.sprite.bounds.size.x;
+        }
+
+        void Update()
+        {
+            if(!leftNeighbour || !rightNeighbour)
+            {
+                float camHor = cam.orthographicSize * Screen.width / Screen.height;
+                float edgeVisibleLeft = (transform.position.x + width / 2) - camHor;
+                float edgeVisibleRight = (transform.position.x - width / 2) - camHor;
+
+                if(cam.transform.position.x >= edgeVisibleRight + offset.x && !rightNeighbour)
+                {
+
+                }
+                else if(cam.transform.position.x <= edgeVisibleRight - offset.x && !leftNeighbour)
+                {
+
+                }
+            }
+        }
+
+        void NewNeighbour (int side)
+        {
+            Vector3 newPos = new Vector3(transform.position.x + width * side, transform.position.y, transform.position.z);
+            Transform neighbour = Instantiate(transform, newPos, transform.rotation) as Transform;
+
+            if (reverseScale)
+                neighbour.localScale = new Vector3(neighbour.localScale.x * -1, neighbour.localScale.y, neighbour.localScale.z);
+
+            neighbour.parent = transform.parent;
+        }
+    }
+}
